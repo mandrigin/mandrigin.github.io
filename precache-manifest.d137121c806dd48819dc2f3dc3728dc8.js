d717a3cf44a9615e50d6c1c6210286be15bc6c0e
Igor Mandrigin <i@mandrigin.ru>
2020-09-29 17:10:11 +0200

    add eth supply demo

---
+++ precache-manifest.d137121c806dd48819dc2f3dc3728dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227612f05f17622d2b6e821603d7d452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf5689936382f734a85",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eab9066e25a6fc4c841c",
+    "url": "/static/js/2.fe56a02c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fe56a02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf5689936382f734a85",
+    "url": "/static/js/main.9acedd55.chunk.js"
+  },
+  {
+    "revision": "076e40bebf555e41d21f",
+    "url": "/static/js/runtime-main.d00f91fc.js"
+  }
+]);
